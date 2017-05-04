9977d85d10b249c93aeace5df0e70b97c05de48a
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-05-04 16:21:32 -0700

    Update build_environment.sh

--- build_environment.sh
+++ build_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This should be the finishing script for a micro with a 50G hard drive
 
-yum -y install rpm-build make gcc
+yum -y install rpm-build make gcc git
 mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cd ~/
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+git clone https://github.com/NagiosEnterprises/nrpe.git
