f53cebcb167d52ca44ebe21456ea730bf48ad856
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-05-04 17:42:22 -0700

    Update source-repos.sh

--- source-repos.sh
+++ source-repos.sh
@@ -11,3 +11,5 @@ rpm --showrc | grep topdir
 # make sure your topdir is pointing somewhere sane
 rpm -ivh ./nrpe-2.15-7.el7.src.rpm 
 rpm -ivh ./nagios-4.2.4-2.el7.src.rpm 
+
+# This will install into your existing build dir in the root home under rpmbuild.
