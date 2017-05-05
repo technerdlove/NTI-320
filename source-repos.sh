2483da9bc0eba48065bd193d16c66682d71eef26
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-05-04 17:41:20 -0700

    Update source-repos.sh

--- source-repos.sh
+++ source-repos.sh
@@ -6,3 +6,8 @@ yumdownloader --source nagios
 
 #This will give you the source packages for nrpe and nagios on centos7
 # Note: this process is much easier on other flavors of linux
+
+rpm --showrc | grep topdir
+# make sure your topdir is pointing somewhere sane
+rpm -ivh ./nrpe-2.15-7.el7.src.rpm 
+rpm -ivh ./nagios-4.2.4-2.el7.src.rpm 
