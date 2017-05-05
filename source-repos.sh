26246a37a107fe4066009df396488fef78ec05f9
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-05-04 17:35:54 -0700

    Update source-repos.sh

--- source-repos.sh
+++ source-repos.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 yum -y install yum-utils rpmbuild
+
+yumdownloader --source nrpe
+yumdownloader --source nagios
+
+#This will give you the source packages for nrpe and nagios on centos7
+# Note: this process is much easier on other flavors of linux
